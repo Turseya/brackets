a356f1c90c8b8d857a403b3dffa390301897d6ba
Turseya <turseya@gmail.com>
2021-03-15 22:18:15 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const closedBrackets = [];
+  const temp = []
+  
+  bracketsConfig.forEach(el => {
+      openBrackets.push(el[0]);
+      closedBrackets.push(el[1]);
+  });
+  
+  for (let i = 0; i < str.length; i++) {
+      const el = str[i];
+  
+      if (closedBrackets.indexOf(el) !== -1) {
+          const openContains = openBrackets[closedBrackets.indexOf(el)];
+          if (temp.length === 0 || (temp.pop() !== openContains)) {
+              return false;
+          }
+      } else {
+          temp.push(el);
+      }
+  }
+  
+  if (temp.length === 0) {
+      return true;
+  } else {
+      return false;
+  }  
 }
